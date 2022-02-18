8e097dde59d210fe9373ed6a0b56228174e51ee4
Berk Özdemir <berk.ozdemir@mobven.com>
2022-02-18 18:13:09 +0300

    ui improvements

--- app/src/main/java/com/example/samplerunproject/MainActivity.kt
+++ app/src/main/java/com/example/samplerunproject/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.util.Log
 import com.example.samplerunproject.api.ApiClient
 import com.example.samplerunproject.base.BaseActivity
+import com.google.android.material.button.MaterialButton
 import dagger.hilt.android.AndroidEntryPoint
 import retrofit2.Call
 import retrofit2.Callback
@@ -17,13 +18,15 @@ class MainActivity : BaseActivity(R.layout.activity_main) {
         ApiClient.getApiService().getLinks("asdsadasdsad").enqueue(object :
             Callback<Response> {
             override fun onResponse(call: Call<Response>, response: retrofit2.Response<Response>) {
-                Log.d("deneme","${response.body()}")
+                Log.d("deneme", "${response.body()}")
             }
 
             override fun onFailure(call: Call<Response>, t: Throwable) {
-                Log.d("deneme","${t.message}")
+                Log.d("deneme", "${t.message}")
             }
         })
+
+        val shortenItButton = findViewById<MaterialButton>(R.id.shorten_it_button)
 
     }
 }
