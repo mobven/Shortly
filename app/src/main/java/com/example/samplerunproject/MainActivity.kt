60b4646793e0dba0c866e9d639393801f9d2b9c5
mehmetgur1 <mehmet.gur@mobven.com>
2022-02-25 18:13:47 +0300

    recyclerView items added

--- app/src/main/java/com/example/samplerunproject/MainActivity.kt
+++ app/src/main/java/com/example/samplerunproject/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.samplerunproject
 import android.os.Bundle
 import android.util.Log
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.example.samplerunproject.adapter.ShortLinkAdapter
 import com.example.samplerunproject.api.ApiClient
 import com.example.samplerunproject.base.BaseActivity
 import com.example.samplerunproject.databinding.ActivityMainBinding
@@ -15,25 +17,27 @@ import retrofit2.Callback
 @AndroidEntryPoint
 class MainActivity : BaseActivity(R.layout.activity_main) {
 
-    val shortenItButton = findViewById<MaterialButton>(R.id.shorten_it_button)
-    val linkText = findViewById<TextInputEditText>(R.id.shorten_link_edt)
-
-    lateinit var binding : ActivityMainBinding
+    private val shortLinkAdapter = ShortLinkAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        val shortenItButton = findViewById<MaterialButton>(R.id.shorten_it_button)
+        val linkText = findViewById<TextInputEditText>(R.id.shorten_link_edt)
+        val rvLinks = findViewById<RecyclerView>(R.id.rv_links)
+
         shortenItButton.setOnClickListener {
             callShortLink(linkText.text.toString())
         }
 
+        rvLinks.adapter = shortLinkAdapter
     }
 
-    fun callShortLink(editLink:String) {
+    private fun callShortLink(editLink:String) {
         ApiClient.getApiService().getLinks(editLink).enqueue(object :
             Callback<Response> {
             override fun onResponse(call: Call<Response>, response: retrofit2.Response<Response>) {
                 Log.d("deneme", "${response.body()}")
+                shortLinkAdapter.setData(mutableListOf(response.body()?.result?.full_share_link ?: ""))
             }
 
             override fun onFailure(call: Call<Response>, t: Throwable) {
--- app/src/main/java/com/example/samplerunproject/adapter/ShortLinkAdapter.kt
+++ app/src/main/java/com/example/samplerunproject/adapter/ShortLinkAdapter.kt
@@ -2,31 +2,41 @@ package com.example.samplerunproject.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
-import com.example.samplerunproject.R
 import com.example.samplerunproject.databinding.ItemShortLinkBinding
 
 class ShortLinkAdapter:RecyclerView.Adapter<ShortLinkAdapter.ViewHolder>() {
 
     private val shortLinkList = mutableListOf<String>()
+    var itemClickListener: (String) -> Unit = {}
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val binding = ItemShortLinkBinding.inflate(LayoutInflater.from(parent.context), parent,false)
+        val binding =
+            ItemShortLinkBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return ViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-       holder.bind(shortLinkList[position])
+        holder.bind(shortLinkList[position])
     }
 
     override fun getItemCount(): Int = shortLinkList.size
 
+    fun setData(list: MutableList<String>) {
+        shortLinkList.clear()
+        shortLinkList.addAll(list)
+        notifyDataSetChanged()
+    }
 
 
-    inner class ViewHolder(val binding:ItemShortLinkBinding):RecyclerView.ViewHolder(binding.root) {
+    inner class ViewHolder(private val binding: ItemShortLinkBinding) :
+        RecyclerView.ViewHolder(binding.root) {
         fun bind(item: String) {
-
+            binding.apply {
+                btnCopy.setOnClickListener {
+                    itemClickListener(tvShortLink.text.toString())
+                }
+            }
         }
     }
 }
