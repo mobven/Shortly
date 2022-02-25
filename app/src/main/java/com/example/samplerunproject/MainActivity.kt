d225da927d8b92f92b3b3a9b41e2f2709d3102cb
Arda Türkoğlu <arda.turkoglu@mobven.com>
2022-02-25 17:46:55 +0300

    binding added.

--- app/src/main/java/com/example/samplerunproject/MainActivity.kt
+++ app/src/main/java/com/example/samplerunproject/MainActivity.kt
@@ -2,20 +2,35 @@ package com.example.samplerunproject
 
 import android.os.Bundle
 import android.util.Log
+import androidx.databinding.DataBindingUtil
 import com.example.samplerunproject.api.ApiClient
 import com.example.samplerunproject.base.BaseActivity
+import com.example.samplerunproject.databinding.ActivityMainBinding
 import com.google.android.material.button.MaterialButton
+import com.google.android.material.textfield.TextInputEditText
 import dagger.hilt.android.AndroidEntryPoint
 import retrofit2.Call
 import retrofit2.Callback
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : BaseActivity(R.layout.activity_main) {
+
+    val shortenItButton = findViewById<MaterialButton>(R.id.shorten_it_button)
+    val linkText = findViewById<TextInputEditText>(R.id.shorten_link_edt)
+
+    lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        shortenItButton.setOnClickListener {
+            callShortLink(linkText.text.toString())
+        }
 
-        ApiClient.getApiService().getLinks("asdsadasdsad").enqueue(object :
+    }
+
+    fun callShortLink(editLink:String) {
+        ApiClient.getApiService().getLinks(editLink).enqueue(object :
             Callback<Response> {
             override fun onResponse(call: Call<Response>, response: retrofit2.Response<Response>) {
                 Log.d("deneme", "${response.body()}")
@@ -25,8 +40,5 @@ class MainActivity : BaseActivity(R.layout.activity_main) {
                 Log.d("deneme", "${t.message}")
             }
         })
-
-        val shortenItButton = findViewById<MaterialButton>(R.id.shorten_it_button)
-
     }
 }
